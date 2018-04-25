a6f0ee8228aaa63641b760fbafa6981d1bc9d97a
pedrovidal <peu_vs@yahoo.com.br>
2018-04-25 11:49:27 -0300

    sistemaPlanetario without groups

--- Lab3-TransformacoesGeometricas/sistemaPlanetario.js
+++ Lab3-TransformacoesGeometricas/sistemaPlanetario.js
@@ -3,6 +3,7 @@ var renderer	= null;
 var camera 		= null;
 var earth 		= null;
 var sun 		= null;
+var moon 		= null;
 var day 		= 0.0;
 var year		= 0.0;
 var month		= 0.0;
@@ -22,17 +23,17 @@ function init() {
 	scene.add( camera );
 		
 	// Eixo do Sol
-	var sAxis = new THREE.AxisHelper(0.6);
+	var sAxis = new THREE.AxesHelper(0.6);
 
 	// Sol
 	var sphereGeometry = new THREE.SphereGeometry( 0.4, 20, 20);                 
 	var sphereMat = new THREE.MeshBasicMaterial( {color: 0xffff00, wireframe:true} );
 	sun = new THREE.Mesh( sphereGeometry, sphereMat );
-	sun.add(sAxis);
+	// sun.add(sAxis);
 	scene.add(sun);	
 	
 	// Eixo da Terra
-	var tAxis = new THREE.AxisHelper(0.15);
+	var tAxis = new THREE.AxesHelper(0.15);
 
 	// Terra
 	
@@ -43,7 +44,7 @@ function init() {
 	scene.add( earth );	
 		
 	// Eixo da Lua
-	var lAxis = new THREE.AxisHelper(0.04);
+	var lAxis = new THREE.AxesHelper(0.04);
 
 	// Lua
 	
@@ -58,17 +59,58 @@ function init() {
 };
 
 function render() {
-	var m = new THREE.Matrix4();
+	var sunMatrix = new THREE.Matrix4();
+	var earthMatrix = new THREE.Matrix4();
+	var moonMatrix = new THREE.Matrix4();
 	
 	day 	+= 0.07;
 	year 	+= 0.01;
 	month 	+= 0.04;
 	
-	m.identity();
-	sun.matrix.copy(m);
-	m.makeRotationY(year);
-	sun.applyMatrix(m);
+	// sun
+	sunMatrix.identity();
+	sun.matrix.copy(sunMatrix);
+
+	sunMatrix.makeRotationY(year);
+	sun.applyMatrix(sunMatrix);
+	
 	sun.updateMatrix();
+
+	// earth
+	earthMatrix.identity();
+	earth.matrix.copy(earthMatrix);
+
+	earthMatrix.makeTranslation(0.0, 0.0, 0.0);
+	earth.applyMatrix(earthMatrix);
+
+	earthMatrix.makeRotationY(day);
+	earth.applyMatrix(earthMatrix);
+
+	earthMatrix.makeTranslation(0.7, 0.0, 0.0);
+	earth.applyMatrix(earthMatrix);
+
+	earthMatrix.makeRotationY(year);
+	earth.applyMatrix(earthMatrix);
+
+	earth.updateMatrix();
+
+	// moon
+	moonMatrix.identity();
+	moon.matrix.copy(moonMatrix);
+
+	moonMatrix.makeTranslation(0.15, 0.0, 0.0);
+	moon.applyMatrix(moonMatrix);
+
+	moonMatrix.makeRotationY(month);
+	moon.applyMatrix(moonMatrix);
+
+	moonMatrix.makeTranslation(0.7, 0.0, 0.0);
+	moon.applyMatrix(moonMatrix);
+
+	moonMatrix.makeRotationY(year);
+	moon.applyMatrix(moonMatrix);
+
+	moon.updateMatrix();
 
 	renderer.render(scene, camera);
 	
