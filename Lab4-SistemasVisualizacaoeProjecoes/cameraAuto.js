0d27005db9ead471c44a4f0642496f1d033544d1
pedrovidal <peu_vs@yahoo.com.br>
2018-05-08 22:42:44 -0300

    Adds trackballs controls to Roteiro4

---
+++ Lab4-SistemasVisualizacaoeProjecoes/cameraAuto.js
@@ -0,0 +1,83 @@
+var scene 		= null;
+var renderer	= null;
+var camera 		= null;
+var angleX		= 0.007;
+var angleY		= 0.0;
+var angleZ		= 0.0;
+var box 		= null;
+
+function init() {
+
+	scene = new THREE.Scene();
+
+	renderer = new THREE.WebGLRenderer();
+
+	renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
+	renderer.setSize(window.innerWidth*0.7, window.innerHeight*0.7);
+	aspectRatio = window.innerWidth/window.innerHeight;
+
+	document.getElementById("WebGL-output").appendChild(renderer.domElement);
+
+	camera = new THREE.PerspectiveCamera( 100.0, aspectRatio, 0.1, 10000.0 );
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	scene.add( camera );
+	
+	// Load Mesh
+	var loader = new THREE.OBJLoader();
+	loader.load('./city.obj', loadMesh);
+		
+	orbitControls = new THREE.TrackballControls(camera/*, renderer.domElement*/);
+	orbitControls.autoRotate = false;
+
+	renderer.clear();
+	render();
+};
+
+
+function render() {
+
+	// var obj = scene.getObjectByName("myObj");
+	// obj.rotateX(angleX);
+	
+	orbitControls.update();
+	renderer.render(scene, camera);
+	requestAnimationFrame(render);
+}
+
+
+function loadMesh(loadedMesh) {
+	
+	loadedMesh.name = "city";
+	var material = new THREE.MeshPhongMaterial();
+	material.bothsides	= false;
+	material.side 		= THREE.FrontSide;
+	
+	loadedMesh.children.forEach(function (child) {
+		child.material = material;
+	});
+
+	mesh = loadedMesh;
+	scene.add(loadedMesh);
+	
+	box = new THREE.Box3();
+	box.expandByObject(mesh);
+	
+	camera.position.set(box.max.x, box.max.y, box.max.z);
+	camera.lookAt(new THREE.Vector3(0.0, 0.0, 0.0));
+	orbitControls.update();
+	
+	//Add point light Source
+	var pointLight1 = new THREE.PointLight(new THREE.Color(1.0, 1.0, 1.0));
+	pointLight1.distance = 0.0;
+	pointLight1.position.set(box.max.x*1.2, box.max.y*1.2, box.max.z*1.2);
+	// pointLight1.position.set(0, 70, 0);
+	scene.add(pointLight1);
+	
+	// Global Axis
+	var globalAxis = new THREE.AxesHelper	( Math.max(	(box.max.x - box.min.x),
+														(box.max.y - box.min.y),
+														(box.max.z - box.min.z)
+				  									  )
+											);
+	scene.add( globalAxis );
+};
