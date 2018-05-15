e2483c10fee6f539440c52b897b068c153ffc786
pedrovidal <peu_vs@yahoo.com.br>
2018-05-15 19:31:36 -0300

    Add viewport to duasCameras

--- Lab4-SistemasVisualizacaoeProjecoes/duasCameras.js
+++ Lab4-SistemasVisualizacaoeProjecoes/duasCameras.js
@@ -1,25 +1,25 @@
 var scene 					= null;
-var rendererPerspective		= null;
-var rendererOrthographic	= null;
+var renderer				= null;
 var cameraPerspective		= null;
+var cameraOrthographic		= null;
 var angleX					= 0.007;
 var angleY					= 0.0;
 var angleZ					= 0.0;
 var box 					= null;
+var zBuffer 				= true;
+var faceCull				= "FrontFace";
+var guiChanged				= false;
+var clock;
+var params;
 
 function init() {
 
 	scene = new THREE.Scene();
 
-	rendererOrthographic = new THREE.WebGLRenderer();
-	rendererOrthographic.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
-	rendererOrthographic.setSize(window.innerWidth*0.5, window.innerHeight*0.5);
-	document.getElementById("WebGL-output").appendChild(rendererOrthographic.domElement);
-
-	rendererPerspective = new THREE.WebGLRenderer();
-	rendererPerspective.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
-	rendererPerspective.setSize(window.innerWidth*0.5, window.innerHeight*0.5);
-	document.getElementById("WebGL-output2").appendChild(rendererPerspective.domElement);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
+	renderer.setSize(window.innerWidth * 0.9, window.innerHeight * 0.9);
+	document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
 	aspectRatio = window.innerWidth/window.innerHeight;
 
@@ -35,40 +35,91 @@ function init() {
 	var loader = new THREE.OBJLoader();
 	loader.load('./city.obj', loadMesh);
 		
-	orbitControlsPerspective = new THREE.OrbitControls(cameraPerspective/*, rendererPerspective.domElement*/);
-	orbitControlsPerspective.autoRotate = false;
+	controlsPerspective = new THREE.FlyControls(cameraPerspective, renderer.domElement);
+	controlsPerspective.autoRotate = false;
 
-	orbitControlsOrthographic = new THREE.OrbitControls(cameraOrthographic/*, rendererOrthographic.domElement*/);
-	orbitControlsOrthographic.autoRotate = false;
+	controlsOrthographic = new THREE.FlyControls(cameraOrthographic, renderer.domElement);
+	controlsOrthographic.autoRotate = false;
 
-	rendererPerspective.clear();
-	renderPerspective();
+	initGUI();
 
-	rendererOrthographic.clear();
-	renderOrthographic();
+	renderer.clear();
+	render();
+
+};
+
+function initGUI() {
+	
+	params = 	{	zBuffer		: true,
+					faceCulling	: "FrontFace"
+				};
+
+	var gui = new dat.GUI();	
+	gui.add( params, 'zBuffer').onChange(function(){
+				guiChanged = true;
+    			});
+	gui.add( params, 'faceCulling', [ "FrontFace", "BackFace", "DoubleSide" ] ).onChange(function(){
+				guiChanged = true;
+    			});
+	gui.open();
+		
 };
 
 
-function renderPerspective() {
+function render() {
 
 	// var obj = scene.getObjectByName("myObj");
 	// obj.rotateX(angleX);
+
+	if (guiChanged) {
+
+		guiChanged = false;
+		
+		if (zBuffer !== params.zBuffer) 
+			zBuffer = params.zBuffer;
+			
+		if (faceCull !== params.faceCulling) 
+			faceCull = params.faceCulling;
+			
+		if (mesh) {
+			scene.getObjectByName("city").children.forEach(function (child) {
+				child.material.depthTest = zBuffer;
+				switch( faceCull ) {
+					case "FrontFace"	: 	child.material.side = THREE.FrontSide; 
+											break;
+					case "BackFace"		: 	child.material.side = THREE.BackSide; 
+											break;
+					case "DoubleSide"	: 	child.material.side = THREE.DoubleSide; 
+											break;
+					}
+				});
+			}
+		}
 	
-	orbitControlsPerspective.update();
-	rendererPerspective.render(scene, cameraPerspective);
-	requestAnimationFrame(renderPerspective);
+	controlsPerspective.update(0.5);
+	controlsOrthographic.update(0.5);
+	// todo viewport
+	// renderer.render(scene, cameraPerspective);
+
+	var left = 0;
+	var top = 0;
+	var width = window.innerWidth * 0.45;
+	var height = window.innerHeight;
+
+	renderer.setViewport(left, top, width, height);
+	renderer.setScissor(left, top, width, height);
+	renderer.setScissorTest(true);    
+	renderer.render(scene, cameraPerspective);
+
+	left = window.innerWidth * 0.45;
+
+	renderer.setViewport(left, top, width, height);
+ 	renderer.setScissor(left, top, width, height);
+	renderer.setScissorTest(true);  
+	renderer.render(scene, cameraOrthographic);
+
+	requestAnimationFrame(render);
 }
-
-function renderOrthographic() {
-
-	// var obj = scene.getObjectByName("myObj");
-	// obj.rotateX(angleX);
-	
-	orbitControlsOrthographic.update();
-	rendererOrthographic.render(scene, cameraOrthographic);
-	requestAnimationFrame(renderOrthographic);
-}
-
 
 function loadMesh(loadedMesh) {
 	
@@ -89,18 +140,19 @@ function loadMesh(loadedMesh) {
 	
 	cameraPerspective.position.set(box.max.x, box.max.y / 2, box.max.z);
 	cameraPerspective.lookAt(new THREE.Vector3(0.0, 0.0, 0.0));
-	orbitControlsPerspective.update();
+	controlsPerspective.update(0.5);
 	
 	cameraOrthographic.bottom = box.min.y;
 	cameraOrthographic.top = box.max.y;
 	cameraOrthographic.left = box.min.x;
 	cameraOrthographic.right = box.max.x;
 	cameraOrthographic.near = 0.1;
-	cameraOrthographic.far = 10000;
+	cameraOrthographic.far = 10000.0;
 	cameraOrthographic.zoom *= 0.5;
 	cameraOrthographic.position.set(box.max.x, box.max.y / 2, box.max.z);
 	cameraOrthographic.lookAt(new THREE.Vector3(0, 0, 0));
 	cameraOrthographic.updateProjectionMatrix();
+	controlsOrthographic.update(0.5);
 
 	//Add point light Source
 	var pointLight1 = new THREE.PointLight(new THREE.Color(1.0, 1.0, 1.0));
