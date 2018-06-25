50712dbf3ab5c53a79adabd1879d5c113606c2b5
pedrovidal <peu_vs@yahoo.com.br>
2018-06-25 18:14:17 -0300

    Add comments and improve readability

--- Roteiro4/equalization.js
+++ Roteiro4/equalization.js
@@ -34,6 +34,7 @@ function init() {
 	
 	var textureLoader = new THREE.TextureLoader();
 
+	// carrega texturas
 	texture = textureLoader.load(/*"../../Assets/Images/ */"lena.png");
 	textureRGB = textureLoader.load(/*"../../Assets/Images/ */"lena.png");
 	textureGrayscale = textureLoader.load(/*"../../Assets/Images/ */"grayscale_lena.png");
@@ -56,6 +57,7 @@ function initGui(){
 	// opcoes de cor
 	var colorGui = gui.addFolder('Color');
 
+	// opcao de imagem em rgb ou em grayscale
 
 	var colorRGBOpt = colorGui.add(colorControls, 'rgb').name('RGB').listen();
     colorRGBOpt.onChange(function(){
@@ -103,6 +105,7 @@ function initGui(){
 
     var equalizationGui = gui.addFolder('Equalization');
 	
+    // opcao de equalizar imagem pelos canais rgb ou equalizar o v
 
 	var equalizationRGBOpt = equalizationGui.add(equalizationControls, 'rgb').name('RGB').listen();
 	equalizationRGBOpt.onChange(function(){
@@ -155,12 +158,16 @@ function getImageData( image ) {
 
 function getPixel( imagedata, x, y ) {
 
+	// recupera canais rgb do pixel (0-255)
+
     var position = ( x + imagedata.width * y ) * 4, data = imagedata.data;
     return new THREE.Color(data[ position ], data[ position + 1], data[ position + 2]);
 
 }
 
-function calcHist(imagedata, isGrayscale){
+function calcHist(imagedata){
+
+	// calculo dos histogramas R, G, B e HSV(apenas para o V)
 
 	for (var i = 0; i < 256; i++){
 		histogramR[i] = 0;
@@ -174,7 +181,7 @@ function calcHist(imagedata, isGrayscale){
 	for (var i = 0; i < imagedata.height; i++){
 		for (var j = 0; j < imagedata.width; j++){
 			var color = getPixel(imagedata, i, j);
-			// console.log(color.r);
+
 			histogramR[Math.round(color.r)]++;
 			histogramG[Math.round(color.g)]++;
 			histogramB[Math.round(color.b)]++;
@@ -194,9 +201,14 @@ function render() {
 		requestAnimationFrame(render);
 	else {
 
+
+		// recupera dados da imagem
 		var imagedata = getImageData( texture.image );
 
+		// opcao de grayscale
 		var grayscale = colorControls.grayscale;
+
+		// carrega textura usando shader
 
 		uniforms = {
 			textureA: { type: "t", value:texture },
@@ -208,40 +220,58 @@ function render() {
 				fragmentShader: document.getElementById( 'base-fs' ).textContent
 			} );
 		
-		// Plane
+		// cria plano
 		var planeGeometry = new THREE.PlaneBufferGeometry(0.8, 0.8, 20, 20);                 
 		var plane = new THREE.Mesh( planeGeometry, matShader );
 		plane.position.set(-0.6, 0.6, -1);
 		scene.add( plane );	
 
+		// checa se opcao grayscale e verdadeira
 		if (grayscale){
 			renderer.setClearColor(new THREE.Color(1.0, 0.0, 0.0));
 
+			// calcula histogramas
+
 			calcHist(imagedata);
+
+			// como os canais R, G e B tem mesmo valor na imagem grayscale
+			// utiliza o histograma calculado para o canal R
 
 			var grayscaleHistogram = histogramR;
 			
-			equalization(imagedata, 'grayscale');
+			// equaliza imagem
+
+			equalization(imagedata);
+
+			// plota histograma da imagem grayscale original
 			
-			plotHistogram(grayscaleHistogram, imagedata.height, imagedata.width, 1, 'grayscale');
+			plotHistogram(grayscaleHistogram, imagedata.width, 1, 'grayscale');
 		}
 
 		else{
 			renderer.setClearColor(new THREE.Color(1.0, 1.0, 1.0));
 
+			// calcula histogramas
+
 			calcHist(imagedata);
+
+			// checa metodo de equalizacao da imagem
 			
+			// se metodo rgb
 			if (equalizationControls.rgb){
-				equalization(imagedata, 'rgb');
+				equalization(imagedata);
 			}
 
+			// senao metodo hsv
 			else if (equalizationControls.hsv){
 				equalizationHSV(imagedata);
 			}
 
-			plotHistogram(histogramR, imagedata.height, imagedata.width, 1, 'r');
-			plotHistogram(histogramG, imagedata.height, imagedata.width, 1, 'g');
-			plotHistogram(histogramB, imagedata.height, imagedata.width, 1, 'b');
+			// plota histogramas R, G e B da imagem RGB original
+
+			plotHistogram(histogramR, imagedata.width, 1, 'r');
+			plotHistogram(histogramG, imagedata.width, 1, 'g');
+			plotHistogram(histogramB, imagedata.width, 1, 'b');
 		}
 		
 		renderer.setSize(imagedata.width*2.5, imagedata.height*2.5);
@@ -249,32 +279,28 @@ function render() {
 		}
 }
 
-function plotHistogram(histogram, height, width, histogramNumber, color){
+function plotHistogram(histogram, width, histogramNumber, color){
 
-	var maxV = - 1, minV = height * width + 1;
+	var maxV = - 1;
 
 	for (var i = 0; i < 256; i++){
 		maxV = Math.max(maxV, histogram[i])
-		minV = Math.min(minV, histogram[i]);	
 	}
 	
-	var barMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
-
 	var initialPos = -1;
 
 	if (histogramNumber == 2){
 		initialPos = 0.3;
-		barMaterial.color = new THREE.Color(0x0000ff);
 	}
 
-	var heightColor = -0.2;
+	var heightOffsetByColor = -0.2;
 
 	if (color == 'g'){
-		heightColor = -0.6;
+		heightOffsetByColor = -0.6;
 	}
 
 	if (color == 'b'){
-		heightColor = -1.0;
+		heightOffsetByColor = -1.0;
 	}
 
 	for (var i = 0; i < 256; i++){
@@ -289,25 +315,29 @@ function plotHistogram(histogram, height, width, histogramNumber, color){
 
 		var barGeometry = new THREE.PlaneGeometry(2/width, barHeight, 1, 1);
 	
-		if (color == 'grayscale')
+		if (color == 'grayscale'){
 			var barMaterial = new THREE.MeshBasicMaterial({color: new THREE.Color(i/255, i/255, i/255)});
-		else if (color == 'r')
+		}
+		else if (color == 'r'){
 			var barMaterial = new THREE.MeshBasicMaterial({color: new THREE.Color(i/255, 0, 0)});
-		else if (color == 'g')
+		}
+		else if (color == 'g'){
 			var barMaterial = new THREE.MeshBasicMaterial({color: new THREE.Color(0, i/255, 0)});
-		else if (color == 'b')
+		}
+		else if (color == 'b'){
 			var barMaterial = new THREE.MeshBasicMaterial({color: new THREE.Color(0, 0, i/255)});
+		}
 
 		var barMesh = new THREE.Mesh(barGeometry, barMaterial);
 	
-		barMesh.position.set(initialPos + ((i / width) * 1.6), heightColor + (barHeight/2) , 0);
+		barMesh.position.set(initialPos + ((i / width) * 1.6), heightOffsetByColor + (barHeight/2) , 0);
 
 		if (color == 'g'){
-			barMesh.position.set(initialPos + ((i / width) * 1.6), heightColor + (barHeight/2) , 0);
+			barMesh.position.set(initialPos + ((i / width) * 1.6), heightOffsetByColor + (barHeight/2) , 0);
 		}
 		
 		if (color == 'b'){
-			barMesh.position.set(initialPos + ((i / width) * 1.6), heightColor + (barHeight/2) , 0);
+			barMesh.position.set(initialPos + ((i / width) * 1.6), heightOffsetByColor + (barHeight/2) , 0);
 		}
 	
 		scene.add(barMesh);
@@ -318,9 +348,9 @@ function equalization(imagedata, color){
 
 	var height = imagedata.height, width = imagedata.width;
 
-	var cR = new Array(256);
-	var cG = new Array(256);
-	var cB = new Array(256);
+	var cdfR = new Array(256);
+	var cdfG = new Array(256);
+	var cdfB = new Array(256);
 	var equalizedR = new Array(256);
 	var equalizedG = new Array(256);
 	var equalizedB = new Array(256);
@@ -331,50 +361,57 @@ function equalization(imagedata, color){
 		equalizedB[i] = 0;
 	}
 
-	cR[0] = histogramR[0];
-	cG[0] = histogramG[0];
-	cB[0] = histogramB[0];
+	// calcula cdf
+
+	cdfR[0] = histogramR[0];
+	cdfG[0] = histogramG[0];
+	cdfB[0] = histogramB[0];
 
 	for (var i = 1; i < 256; i++){
-		cR[i] = histogramR[i] + cR[i - 1];
-		cG[i] = histogramG[i] + cG[i - 1];
-		cB[i] = histogramB[i] + cB[i - 1];
+		cdfR[i] = histogramR[i] + cdfR[i - 1];
+		cdfG[i] = histogramG[i] + cdfG[i - 1];
+		cdfB[i] = histogramB[i] + cdfB[i - 1];
 	}
 
 	for (var i = 1; i < 256; i++){
-		cR[i] /= height * width;
-		cG[i] /= height * width;
-		cB[i] /= height * width;
+		cdfR[i] /= height * width;
+		cdfG[i] /= height * width;
+		cdfB[i] /= height * width;
 	}
-	
-	// console.log(height, width, height * width)
 
 	const pixelValues = [];
 
 	for (var i = 0; i < height; i++){
 		for (var j = 0; j < width ; j++){
-			var colorR = cR[Math.round(getPixel(imagedata, i, j).r)]*255;
-			var colorG = cG[Math.round(getPixel(imagedata, i, j).g)]*255;
-			var colorB = cB[Math.round(getPixel(imagedata, i, j).b)]*255;
-			// console.log(color)
-			if (color == 'grayscale'){
-				colorG = colorB = colorR = 0.299 * colorR + 0.587 * colorG + 0.114 * colorB;
-			}
+			var colorR = cdfR[Math.round(getPixel(imagedata, i, j).r)]*255;
+			var colorG = cdfG[Math.round(getPixel(imagedata, i, j).g)]*255;
+			var colorB = cdfB[Math.round(getPixel(imagedata, i, j).b)]*255;
+
+			// calcula novos histogramas da imagem equalizada
+
 			equalizedR[Math.round(colorR)]++;
 			equalizedG[Math.round(colorG)]++;
 			equalizedB[Math.round(colorB)]++;
+
+			// guarda valores dos novos pixeis (imagem equalizada)
+
 			pixelValues.push(colorR, colorG, colorB);
 		}
 	}
 
-	if (color == 'grayscale'){
-		plotHistogram(equalizedR, height, width, 2, 'grayscale');
+	// plota histogramas equalizados
+
+	if (colorControls.grayscale){
+		plotHistogram(equalizedR, width, 2, 'grayscale');
 	}
 	else{
-		plotHistogram(equalizedR, height, width, 2, 'r');
-		plotHistogram(equalizedG, height, width, 2, 'g');
-		plotHistogram(equalizedB, height, width, 2, 'b');
+		plotHistogram(equalizedR, width, 2, 'r');
+		plotHistogram(equalizedG, width, 2, 'g');
+		plotHistogram(equalizedB, width, 2, 'b');
 	}
+
+	// carrega imagem equalizada
+
 	equalizedTexture = new THREE.DataTexture( Uint8Array.from(pixelValues), width, height, THREE.RGBFormat );
 	equalizedTexture.needsUpdate = true;
 
@@ -389,7 +426,6 @@ function equalization(imagedata, color){
 		fragmentShader: document.getElementById( 'base-fs' ).textContent
 	} );
 
-	// Plane
 	var planeGeometry = new THREE.PlaneBufferGeometry(0.8, 0.8, 20, 20);                
 	var equalizedPlane = new THREE.Mesh( planeGeometry, matShader );
 	equalizedPlane.position.set(0.7, 0.6, -0.5);
@@ -399,6 +435,8 @@ function equalization(imagedata, color){
 }
 
 function rgbToHsv(colorRGB){
+	// conversao rgb para hsv
+
 	var colorHSV = colorRGB;
 	var rl = colorRGB.r / 255;
 	var gl = colorRGB.g / 255;
@@ -445,6 +483,8 @@ function rgbToHsv(colorRGB){
 }
 
 function hsvToRgb(colorHSV){
+	// conversao hsv para rgb
+
 	var colorRGB = colorHSV;
 
 	var h = colorHSV.r;
@@ -491,8 +531,10 @@ function hsvToRgb(colorHSV){
 
 function equalizationHSV(imagedata){
 
+	// equalizacao no V
+
 	var height = imagedata.height, width = imagedata.width;
-	var c = new Array(256);
+	var cdf = new Array(256);
 	var equalizedR = new Array(256);
 	var equalizedG = new Array(256);
 	var equalizedB = new Array(256);
@@ -503,16 +545,18 @@ function equalizationHSV(imagedata){
 		equalizedB[i] = 0;
 	}
 
-	c[0] = histogramHSV[0];
+	cdf[0] = histogramHSV[0];
 
 	for (var i = 1; i < 256; i++){
-		c[i] = histogramHSV[i] + c[i - 1];
+		cdf[i] = histogramHSV[i] + cdf[i - 1];
 	}
 
 	for (var i = 1; i < 256; i++){
-		c[i] /= height * width;
+		cdf[i] /= height * width;
 	}
 	
+	// vetor para guardar novos pixeis
+
 	const pixelValues = [];
 
 	var colorRGB = new THREE.Color(0, 0, 0);
@@ -524,27 +568,40 @@ function equalizationHSV(imagedata){
 			colorRGB.g = getPixel(imagedata, i, j).g;
 			colorRGB.b = getPixel(imagedata, i, j).b;
 			
+			// converte de rgb para hsv
+
 			colorHSV = rgbToHsv(colorRGB);
 
-			colorHSV.b = c[Math.round(colorHSV.b * 255)];
+			// calcula novo valor de v com base na equalizacao
+
+			colorHSV.b = cdf[Math.round(colorHSV.b * 255)];
+
+			// converte de hsv para rgb
 
 			colorRGB = hsvToRgb(colorHSV);
+
+			// calcula histogramas equalizados
 
 			equalizedR[Math.round(colorRGB.r)]++;
 			equalizedG[Math.round(colorRGB.g)]++;
 			equalizedB[Math.round(colorRGB.b)]++;
 
+			// guarda novos valores de pixeis
+
 			pixelValues.push(colorRGB.r, colorRGB.g, colorRGB.b);
 		}
 	}
 
-	plotHistogram(equalizedR, height, width, 2, 'r');
-	plotHistogram(equalizedG, height, width, 2, 'g');
-	plotHistogram(equalizedB, height, width, 2, 'b');
+	// plota histogramas equalizados
+
+	plotHistogram(equalizedR, width, 2, 'r');
+	plotHistogram(equalizedG, width, 2, 'g');
+	plotHistogram(equalizedB, width, 2, 'b');
+
+	// carrega imagem equalizada
 	
 	equalizedTexture = new THREE.DataTexture( Uint8Array.from(pixelValues), width, height, THREE.RGBFormat );
 	equalizedTexture.needsUpdate = true;
-
 
 	uniforms = {
 		textureA: { type: "t", value:equalizedTexture }
@@ -556,7 +613,6 @@ function equalizationHSV(imagedata){
 		fragmentShader: document.getElementById( 'base-fs' ).textContent
 	} );
 
-	// Plane
 	var planeGeometry = new THREE.PlaneBufferGeometry(0.8, 0.8, 20, 20);                
 	var equalizedPlane = new THREE.Mesh( planeGeometry, matShader );
 	equalizedPlane.position.set(0.7, 0.6, -0.5);
